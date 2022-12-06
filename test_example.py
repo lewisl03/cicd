f194a9773b78b22dfe62138bde5e0ed5b17e4c76
Lewis <lewislingham@gmail.com>
2022-12-06 16:06:27 +0000

    black formatting

--- test_example.py
+++ test_example.py
@@ -1,9 +1,11 @@
 from example import *
 
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -2) == -1
     assert add(1000000000000000000, 2) == 1000000000000000002
 
+
 def test_multiply():
-    assert 2*3 == 6
+    assert 2 * 3 == 6
